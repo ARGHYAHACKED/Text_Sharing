37ea7d060ac7b6ad3657cb8bc109c5f011df604f
ARGHYA <samantaarghya351@gmail.com>
2025-03-17 20:22:07 +0530

    Updated API URL for deployment in thr frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL, 'https://text-sharing-nine.vercel.app'],
   credentials: true
 }));
 
