96499c04a6ba6f91455910d0ea0ebaaf0df5d2e3
ARGHYA <samantaarghya351@gmail.com>
2025-03-18 15:22:57 +0530

    ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/texts', textRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
